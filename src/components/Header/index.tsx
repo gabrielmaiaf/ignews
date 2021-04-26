f5ba78017bfac44fe0e20cf90108e7a1155f43fd
Gabriel Maia <gabrielmaia@protonmail.com>
2021-04-26 18:37:55 +0100

    Added prismic CMS integration

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { SignInButton } from "../SignInButton";
+import { ActiveLink } from "../ActiveLink";
 
 import styles from "./styles.module.scss";
 
@@ -8,8 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
